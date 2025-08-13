9c91f734ddac753de38eea16c3f0fa667ef1e702
Mohammedalilgrh <164587973+Mohammedalilgrh@users.noreply.github.com>
2025-08-13 18:42:02 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from flask import Flask, render_template
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 import json
